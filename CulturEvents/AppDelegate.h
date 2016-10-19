8bd7cf82a73e46c8682542d57e8a29a71d20c77c
Vladimir Bolotov <tastex@MacBook-Air.local>
2016-10-19 20:29:47 +0300

    Initial Commit

---
+++ CulturEvents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CulturEvents
+//
+//  Created by Vladimir Bolotov on 19.10.16.
+//  Copyright © 2016 Vladimir Bolotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CulturEvents/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CulturEvents
+//
+//  Created by Vladimir Bolotov on 19.10.16.
+//  Copyright © 2016 Vladimir Bolotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
